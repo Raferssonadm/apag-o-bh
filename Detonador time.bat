a26de6590bcbe0113027f5fa39eb13cc84204803
Raferssonadm <139918968+Raferssonadm@users.noreply.github.com>
2024-10-27 03:32:32 -0300

    Add files via upload

---
+++ Detonador time.bat
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 104.27.184.94
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Random num = new Random();
+            string pergunta
+                while(true)
+
+                Console.WriteLine("fale uma pergunta...")
+               pergunta = Console.ReadLine
+                aleatorio = num.Next ()
+ time.readkey
+                break;
+
+
+        }
+        Console.ReadKey();
+    }
+}
